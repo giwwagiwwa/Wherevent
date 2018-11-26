75613917be6982eb236bdd35abc1b883b9c274ce
miquel.egea <miquelegea@gmail.com>
2018-11-26 13:02:33 +0100

    3. Llenar modelo Confirmación y Quedada

--- app/src/main/java/com/miquel/egea/wherevent/Quedada.java
+++ app/src/main/java/com/miquel/egea/wherevent/Quedada.java
@@ -1,14 +1,73 @@
 package com.miquel.egea.wherevent;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.util.List;
 
-public class Quedada extends AppCompatActivity {
-    //modelo datos Quedada
+public class Quedada {
+    String Titulo, Descripción, Ubicacion, Fecha, Hora;
+    Float Identificador, TipoEvento;
+    List<Confirmacion> confirmaciones;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public String getTitulo() {
+        return Titulo;
+    }
 
-        super.onCreate(savedInstanceState);
+    public void setTitulo(String titulo) {
+        Titulo = titulo;
+    }
+
+    public String getDescripción() {
+        return Descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        Descripción = descripción;
+    }
+
+    public String getUbicacion() {
+        return Ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        Ubicacion = ubicacion;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
+
+    public String getHora() {
+        return Hora;
+    }
+
+    public void setHora(String hora) {
+        Hora = hora;
+    }
+
+    public Float getIdentificador() {
+        return Identificador;
+    }
+
+    public void setIdentificador(Float identificador) {
+        Identificador = identificador;
+    }
+
+    public Float getTipoEvento() {
+        return TipoEvento;
+    }
+
+    public void setTipoEvento(Float tipoEvento) {
+        TipoEvento = tipoEvento;
+    }
+
+    public List<Confirmacion> getConfirmaciones() {
+        return confirmaciones;
+    }
+
+    public void setConfirmaciones(List<Confirmacion> confirmaciones) {
+        this.confirmaciones = confirmaciones;
     }
 }
