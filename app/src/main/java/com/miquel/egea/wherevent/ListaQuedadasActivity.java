9e67c10e2b9d39ab43b652c7b9a7f28f63a65ab3
miquel.egea <miquelegea@gmail.com>
2018-12-03 13:32:47 +0100

    4.4.1 Cambio icono y sync con NewquedadaActivity +layout

--- app/src/main/java/com/miquel/egea/wherevent/ListaQuedadasActivity.java
+++ app/src/main/java/com/miquel/egea/wherevent/ListaQuedadasActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -91,6 +92,15 @@ public class ListaQuedadasActivity extends AppCompatActivity {
 
 
     //Recycler view
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 
 
 
