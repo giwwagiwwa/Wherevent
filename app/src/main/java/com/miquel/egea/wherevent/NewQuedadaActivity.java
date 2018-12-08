023e3a24416d8bbaf7aa54fb84fa0a2e771fd9f3
Enrique Morral <quique.morral@gmail.com>
2018-12-08 16:01:34 +0100

    10. Selector de Fecha y selector de Hora

--- app/src/main/java/com/miquel/egea/wherevent/NewQuedadaActivity.java
+++ app/src/main/java/com/miquel/egea/wherevent/NewQuedadaActivity.java
@@ -1,31 +1,40 @@
 package com.miquel.egea.wherevent;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.media.Image;
-import android.provider.ContactsContract;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-import java.util.List;
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
 
 public class NewQuedadaActivity extends AppCompatActivity {
     EditText tituloedit;
     EditText ubicacionedit;
-    EditText fechaedit;
-    EditText horaedit;
+    TextView fechaedit;
+    TextView horaedit;
     EditText descripcionedit;
     Integer tipoevento=0;
+    private DatePickerDialog.OnDateSetListener fechaeditListener;
+    private TimePickerDialog.OnTimeSetListener horaeditListener;
     private static int iconos[] = { R.drawable.bbq, R.drawable.bolos, R.drawable.camping, R.drawable.cena, R.drawable.cine, R.drawable.copa,
             R.drawable.estudio,R.drawable.globos, R.drawable.gym, R.drawable.pastel, R.drawable.playa, R.drawable.regalo,
             R.drawable.viaje};
@@ -41,6 +50,61 @@ public class NewQuedadaActivity extends AppCompatActivity {
         horaedit = this.findViewById(R.id.horaedit);
         descripcionedit = this.findViewById(R.id.descripcionedit);
 
+        fechaedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        NewQuedadaActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        fechaeditListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        fechaeditListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = day + "/" + month + "/" + year;
+                fechaedit.setText(date);
+            }
+        };
+
+        horaedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int hora = cal.get(Calendar.HOUR);
+                int minutos = cal.get(Calendar.MINUTE);
+
+                TimePickerDialog dialog = new TimePickerDialog(
+                        NewQuedadaActivity.this,
+                        android.R.style.Theme_Holo_Dialog_MinWidth,
+                        horaeditListener,
+                        hora, minutos, true);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        horaeditListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker datePicker, int hora, int minutos) {
+                String time = hora + ":" + minutos + " h.";
+                horaedit.setText(time);
+            }
+        };
+
+
+
+
         RecyclerView mylist = findViewById(R.id.recyclerView2);
         //creamos el layout HORIZONTAL
         LinearLayoutManager layoutManager
@@ -48,6 +112,8 @@ public class NewQuedadaActivity extends AppCompatActivity {
         mylist.setLayoutManager(layoutManager);
         mylist.setAdapter(new MyAdapter());
     }
+
+
 
     //recycler view iconos
     class ViewHolder extends RecyclerView.ViewHolder{
@@ -110,4 +176,6 @@ public class NewQuedadaActivity extends AppCompatActivity {
         setResult(RESULT_OK,data);
         finish();
     }
+
+
 }
