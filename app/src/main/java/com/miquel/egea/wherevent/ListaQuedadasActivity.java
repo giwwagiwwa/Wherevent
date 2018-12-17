7d09b40baa91ae371d9b78324877471891d53d4e
bilal.el.mahdali <bilalafri@gmail.com>
2018-12-17 11:39:46 +0100

    menu ranking

--- app/src/main/java/com/miquel/egea/wherevent/ListaQuedadasActivity.java
+++ app/src/main/java/com/miquel/egea/wherevent/ListaQuedadasActivity.java
@@ -135,7 +135,7 @@ public class ListaQuedadasActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_ranking) {
             return true;
         }
 
